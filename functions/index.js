5bbb8755298e3534b1027f37ae5e42be19c1761c
Kurt Weston <kurt.weston97@gmail.com>
2024-02-22 22:08:16 +1300

    Added and tested basic function extension

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,7 @@
-/*
- * This template contains a HTTP function that
- * responds with a greeting when called
- *
- * Reference PARAMETERS in your functions code with:
- * `process.env.<parameter-name>`
- * Learn more about building extensions in the docs:
- * https://firebase.google.com/docs/extensions/publishers
- */
+// Import the firebase-functions package using ES module syntax
+import * as functions from "firebase-functions";
 
-const functions = require("firebase-functions");
-
-exports.greetTheWorld = functions.https.onRequest((req, res) => {
+export const greetTheWorld = functions.https.onRequest((req, res) => {
   // Here we reference a user-provided parameter
   // (its value is provided by the user during installation)
   const consumerProvidedGreeting = process.env.GREETING;
@@ -19,7 +10,7 @@ exports.greetTheWorld = functions.https.onRequest((req, res) => {
   // (its value is provided by Firebase after installation)
   const instanceId = process.env.EXT_INSTANCE_ID;
 
-  const greeting = `${consumerProvidedGreeting} World from ${instanceId}`;
+  const greeting = `Hello World from ${instanceId}`;
 
   res.send(greeting);
 });
